be28382be996cd2431bd924981933830142b0f6b
racksonree-prog <racksonree@gmail.com>
2026-06-10 00:21:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const menuButton = document.getElementById("menuButton");
+const navLinks = document.getElementById("navLinks");
 
+menuButton.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
+
+const links = navLinks.querySelectorAll("a");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("open");
+  });
+});
